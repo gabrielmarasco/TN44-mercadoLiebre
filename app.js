6754f6d5afbf174b40bbca095f88b208df628ad0
gabriel_marasco <gamarasco4@gmail.com>
2022-10-08 15:34:46 -0300

    agruegue app y publics

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const publicPath = path.resolve(__dirname, './public')
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
+
+app.use(express.static('public'))
+
+app.listen(PORT, ()=>console.log('servidor corriendo en puerto 3000'))
+
+
+ 
+app.get('/register', (req,res)=>{
+     res.sendFile(path.join(__dirname, './views/register.html'))
+})
+app.get('/login', (req,res)=>{
+     res.sendFile(path.join(__dirname, './views/login.html'))
+})
